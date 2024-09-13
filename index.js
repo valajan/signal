e65d10e2d3ed0dd2887a6bf4cb8ce6f7186149ea
Valarane <s.anth@outlook.fr>
2024-09-13 23:32:47 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('message', (message) => {
+    // Relay messages between peers
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+console.log('Signal service running on ws://localhost:8080');
